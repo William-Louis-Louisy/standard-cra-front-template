10c675492de809a90e7708d4d308d0aea008ee13
William <wlouislouisy@gmail.com>
2022-03-12 16:45:57 +0100

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header({ onClickFunction }) {
+  return (
+    <div className=" z-50 w-full h-14 fixed shadow-lg px-8 flex flex-row justify-between items-center bg-white"></div>
+  );
+}
